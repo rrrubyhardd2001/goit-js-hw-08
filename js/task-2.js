3c52f9c17fc99f6074aba3e2bf1b7ceb4618f7f6
rubyddah <hardd2001@gmail.com>
2024-06-25 00:57:02 +0200

    add js

--- js/task-2.js
+++ js/task-2.js
@@ -1,24 +1,13 @@
-// function getShippingMessage(country, price, deliveryFee) {
-//   const totalPrice = price + deliveryFee;
-//   return `Shipping to ${country} will cost ${totalPrice} credits`;
-// }
-
-// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
-
-function formatMessage(message, maxLength) {
-  let widthMesage = message.length;
-  if (widthMesage <= maxLength) {
-    return message;
-  } else if (widthMesage > maxLength) {
-    return message.slice(0, maxLength) + "...";
+function makeArray(firstArray, secondArray, maxLength) {
+  let thirdArray = firstArray.concat(secondArray);
+  if (thirdArray.length >= maxLength) {
+    return thirdArray.slice(0, maxLength);
   }
 }
 
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
--- js/task-3.js
+++ js/task-3.js
@@ -1,26 +1,15 @@
-// function getElementWidth(content, padding, border) {
-//   let conten = parseFloat(content);
-//   let paddin = parseFloat(padding);
-//   let borde = parseFloat(border);
-//   let totalwidth = conten + (paddin + borde) * 2;
-//   return totalwidth;
-// }
-
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
-
-function checkForSpam(message) {
-  let registr = message.toUpperCase();
-  return (
-    registr.includes("spam".toUpperCase()) ||
-    registr.includes("sale".toUpperCase())
-  );
+function filterArray(numbers, value) {
+  let arrNumbers = [];
+  for (i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      arrNumbers.push(numbers[i]);
+    }
+  }
+  return arrNumbers;
 }
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
