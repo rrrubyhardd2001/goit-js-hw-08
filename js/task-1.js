a2132b39fe2590ad8ece1c2e244c62b84b5b40fe
rubyddah <hardd2001@gmail.com>
2024-07-30 17:20:20 +0200

    add js and css

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const myArtefacts = document.querySelectorAll("#categories .item");
-console.log(`Number of Categories: ${myArtefacts.length}`);
-myArtefacts.forEach((artefact) => {
-  let titleArtefact = artefact.children[0].textContent;
-  console.log(`Category: ${titleArtefact}`);
-  let widthArtefact = artefact.children[1].children.length;
-  console.log(`Elements: ${widthArtefact}`);
-});
--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const inputFolder = document.querySelector("#name-input");
-const outputFolder = document.querySelector("#name-output");
-console.log(inputFolder);
-console.log(outputFolder);
-inputFolder.addEventListener("input", () => {
-  const rezult = inputFolder.value.trim();
-  outputFolder.textContent = rezult;
-});
